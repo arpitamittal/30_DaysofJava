b99331f1fb1cad063f1c053064b76dff6ae70570
Arpita <arpitamittal66@gmail.com>
2021-04-20 22:40:29 +0530

    Task 1 in Java

---
+++ task1/student.java
@@ -0,0 +1,25 @@
+package task1;
+
+public class  student {
+        public String sName;
+        public String sSection;
+        public String sGrade;
+        public String getStudentName() {
+            return sName;
+        }
+        public void setStudentName(String studentName) {
+            this.sName = studentName;
+        }
+        public String getStudentSection() {
+            return sSection;
+        }
+        public void setStudentSection(String studentSection) {
+            this.sSection = studentSection;
+        }
+        public String getStudentGrade() {
+            return sGrade;
+        }
+        public void setStudentGrade(String studentGrade) {
+            this.sGrade = studentGrade;
+        }
+}
